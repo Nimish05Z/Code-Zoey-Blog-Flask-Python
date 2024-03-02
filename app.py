2d0a023d2bcdf1e70783dafad7f276162f0e78ca
Nimish05Z <nimish.garg@kalvium.community>
2024-03-02 15:45:18 +0530

    uncommenting app run command

--- app.py
+++ app.py
@@ -169,5 +169,5 @@ def dashboard():
         
     return render_template('login.html', params=params)
 
-# if __name__ == "__main__":
-#     app.run(debug=True)
+if __name__ == "__main__":
+    app.run(debug=True)
